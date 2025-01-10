b954fe94b693b4c63d1e33f379957e21a784a2b6
nata-lieeee13 <730484@student.inholland.nl>
2025-01-10 18:48:40 +0100

    Add files via upload

---
+++ week 4 part 1/Assignment 3/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Create an array that holds 3 integers
+        int[] numbers = new int[3];
+
+        //Ask the user for there input
+        Console.WriteLine("Enter 3 integers: ");
+
+        Program program = new Program();
+        program.FillArray(numbers);
+
+        int sum= CalculateSum(numbers);
+
+        double average= CalculateAverage(numbers);
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.ReadKey();
+
+    }
+
+    // Fills array with number entered by user 
+    void FillArray(int[] numbers)
+    {
+        for(int i  = 0; i < numbers.Length; i++)
+        {
+           numbers[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    //Calculate the sum 
+    static int CalculateSum(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    //Avergae of the elements
+    static double CalculateAverage(int[] numbers)
+    {
+        int sum = CalculateSum(numbers);
+        return (double)sum / numbers.Length;
+    }
+} 
+
---
+++ week 4 part 1/Assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Assignment1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.start();
+    }
+
+    void start()
+    {
+        //create an array that holds 20 integer elements 
+
+        int[] numbers = new int[20];
+
+        FillArray(numbers);
+
+        DisplayArray(numbers);
+
+        Console.ReadKey();
+
+    }
+
+    // Create a method that fills the array with random numbers
+    void FillArray(int[] numbers)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 100);
+        }
+
+    }
+
+    //Display the array with random numbers
+    void DisplayArray(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Element {i + 1} is: {numbers[i]}");
+        }
+    }
+
+}
+
+
+
