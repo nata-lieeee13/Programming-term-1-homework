055345ddc3bd84ed0da3daa9d068cd94468d42f9
nata-lieeee13 <730484@student.inholland.nl>
2025-01-10 18:57:06 +0100

    Add files via upload

---
+++ week 6 part 3/Assignment7/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assignment7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.start();
+    }
+
+    void start()
+    {
+        //Create an object;
+        Product product = new Product("Laptop", 1200);
+
+        //Displaying the information
+        Console.WriteLine($"Product Name: {product.Name}");
+        Console.WriteLine($"Product Price: {product.Price}");
+
+        Console.ReadKey();
+    }
+}
+
+class Product
+{
+    //Properties with private setter for price
+    public string Name { get; set; }
+    public double Price { get; private set; }
+
+    //Constructors to intialize
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
+
---
+++ week 6 part 3/Assignment8/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Assignment8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.start();
+    }
+
+    void start()
+    {
+        //Create a BankAccount object
+        BankAccount account = new BankAccount("123456789",1000);
+
+        //Display the intial details
+        Console.WriteLine($"Account Number: {account.AccountNumber}");
+        Console.WriteLine($"Initial Balance: {account.Balance}");
+
+        //Deposit the money
+        Console.Write("Enter deposit amount: ");
+        double depositAmount = double.Parse(Console.ReadLine());
+        account.Deposit(depositAmount);
+
+        //Withdraw money
+        Console.Write("Enter withdrawl amount: ");
+        double withdrawlAmount = double.Parse(Console.ReadLine());
+        account.Withdrawl(withdrawlAmount);
+
+        //Display final balance
+        Console.WriteLine($"Final Balance: {account.Balance}");
+
+        Console.ReadKey();
+    }
+}
+
+class BankAccount
+{
+    public string AccountNumber { get; private set; }
+    public double Balance { get; private set; }
+
+    public BankAccount(string accountNumber, double initialBalance)
+    {
+        AccountNumber = accountNumber;
+        Balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if(amount >0)
+        {
+            Balance += amount;
+            Console.WriteLine("Deposit Successful!");
+        }
+        else
+        {
+            Console.WriteLine("Deposit Failed");
+        }
+    }
+
+    public void Withdrawl(double amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine("Withdrawl Succesful!");
+        }
+        else
+        {
+            Console.WriteLine("Withdrawl failed");
+        }
+    }
+}
+
+
