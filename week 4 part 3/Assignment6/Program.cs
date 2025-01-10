32d6989c877ddc67cbf08ce9d5aee48bf09fd49e
nata-lieeee13 <730484@student.inholland.nl>
2025-01-10 18:49:37 +0100

    Add files via upload

---
+++ week 4 part 3/Assignment6/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.start();
+    }
+
+    void start()
+    {
+        //create an arrray that store 6 values
+        int[] diceCounts = new int [6];
+
+        //Intialize all elements in this array with value 0.
+        ThrowDice(diceCounts, 6000);
+
+        for (int i = 0; i < diceCounts.Length; i++)
+        {
+            Console.WriteLine($"Number of throws of value {i + 1}: {diceCounts[i]}");
+        }
+
+        Console.ReadKey();
+    }
+
+    //Generate the number of throws 
+    void ThrowDice(int[] diceCounts, int numberOfThrows)
+    {
+        Random random = new Random();
+
+        for( int i = 0; i < numberOfThrows; i++ )
+        {
+            int diceValue = random.Next(1, 7);
+
+            diceCounts [diceValue - 1]++;
+
+        }
+
+    }
+}
+
---
+++ week 4 part 3/Assignment7/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Assignment7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Start();
+    }
+
+    void Start()
+    {
+        //Create the array for booking the seat 
+        bool[] seats = new bool[10];
+
+        //Starting availability of seats
+        InitializeSeats(seats);
+
+        DisplaySeatingArrangement(seats);
+
+        //Get the user to choose the seat they want 
+        Console.Write("Enter the seat number to book (1-10): ");
+        int seatNumber = int.Parse(Console.ReadLine());
+
+        //Ending availability of seats
+        BookSeat(seats, seatNumber);
+
+        DisplaySeatingArrangement(seats);
+
+        Console.ReadKey();
+
+    }
+
+    //See if the seat is booked or available
+    void InitializeSeats(bool[] seats)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < seats.Length; i++)
+        {
+            seats[i] = random.Next(2) == 0; 
+        }
+    }
+
+    void DisplaySeatingArrangement(bool[] seats)
+    {
+        Console.WriteLine("Current seating arrangement:");
+        for (int i = 0; i < seats.Length; i++)
+        {
+            Console.WriteLine($"Seat {i + 1}: {(seats[i] ? "Available" : "Booked")}");
+        }    
+    }
+
+    void BookSeat(bool[] seats, int seatNumber)
+    {
+        int index = seatNumber - 1;
+        //Marks the seat number as booked(false) in the seats array
+        //Gives a warning if the seat is already booked
+        if (seats[index])
+        {
+            seats[index] = false; 
+            Console.WriteLine($"Seat {seatNumber} has been successfully booked.");
+        }
+        else
+        {
+            Console.WriteLine($"Seat {seatNumber} is already booked. Please choose another seat.");
+        }
+    }
+}
+
